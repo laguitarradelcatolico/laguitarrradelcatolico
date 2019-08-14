3780ad584805f613fda77c6c638e3be3c5c8a153
guitarraenfull <laguitarradelcatolico@gmail.com>
2019-08-14 16:07:58 -0500

    subiendo repositorio final

---
+++ js/epayco.js
@@ -0,0 +1,154 @@
+function enviar_formulario(){ 
+   link.frm_botonePayco.submit() 
+	link.onmousedown = "";
+	
+	<form align="center"  id="frm_botonePayco" name="frm_botonePayco" method="post" action="https://secure.payco.co/checkout.php"> 
+  <input name="p_cust_id_cliente" type="hidden" value="25223"/>
+  <input name="p_key" type="hidden" value="f36e9149f485659f0a1a9b14158d528175829a9e"/>
+  <input name="p_id_invoice" type="hidden" value=""/>
+  <input name="p_description" type="hidden" value="PAGO POR APOYO MUSICAL INDEPENDIENTE"/>
+  <input name="p_currency_code" type="hidden" value="USD"/>
+  <input name="p_amount" id="p_amount" type="hidden" value="10.00"/>
+  <input name="p_tax" id="p_tax" type="hidden" value="0"/>
+  <input name="p_amount_base" id="p_amount_base" type="hidden" value="0"/>
+  <input name="p_test_request" type="hidden" value="FALSE"/>
+  <input name="p_url_response" type="hidden" value=""/> 
+  <input name="p_url_confirmation" type="hidden" value=""/> 
+    <input name="p_signature" type="hidden" id="signature"  value="bd957221d57dc4d6227adf052ccfe288" />
+    <input name="idboton" type="hidden" id="idboton"  value="7254" />  
+    <input width="200" height="200" type="image" id="imagen" src="" /> </form>
+	
+	
+	
+	
+	
+	
+} 
+function enviar_formulario1(){ 
+   document.frm_botonePayco1.submit() 
+} 
+function enviar_formulario2(){ 
+   document.frm_botonePayco2.submit() 
+} 
+function enviar_formulario3(){ 
+   document.frm_botonePayco3.submit() 
+} 
+function enviar_formulario4(){ 
+   document.frm_botonePayco4.submit() 
+} 
+function enviar_formulario5(){ 
+   document.frm_botonePayco5.submit() 
+} 
+
+
+
+<!--10 DOLARES-->
+	
+	<form align="center"  id="frm_botonePayco" name="frm_botonePayco" method="post" action="https://secure.payco.co/checkout.php"> 
+  <input name="p_cust_id_cliente" type="hidden" value="25223"/>
+  <input name="p_key" type="hidden" value="f36e9149f485659f0a1a9b14158d528175829a9e"/>
+  <input name="p_id_invoice" type="hidden" value=""/>
+  <input name="p_description" type="hidden" value="PAGO POR APOYO MUSICAL INDEPENDIENTE"/>
+  <input name="p_currency_code" type="hidden" value="USD"/>
+  <input name="p_amount" id="p_amount" type="hidden" value="10.00"/>
+  <input name="p_tax" id="p_tax" type="hidden" value="0"/>
+  <input name="p_amount_base" id="p_amount_base" type="hidden" value="0"/>
+  <input name="p_test_request" type="hidden" value="FALSE"/>
+  <input name="p_url_response" type="hidden" value=""/> 
+  <input name="p_url_confirmation" type="hidden" value=""/> 
+    <input name="p_signature" type="hidden" id="signature"  value="bd957221d57dc4d6227adf052ccfe288" />
+    <input name="idboton" type="hidden" id="idboton"  value="7254" />  
+    <input width="200" height="200" type="image" id="imagen" src="https://1.bp.blogspot.com/-NlpxlN5x6Io/XToGRWbMJwI/AAAAAAAAEw4/Cha0pcxMQrMZz0Jnz_nyU9x2tZ6uCYgiQCLcBGAs/s320/BOTON%2BAPOYO10.png" />
+</form>
+
+<!--20 DOLARES-->
+	<form align="center" id="frm_botonePayco" name="frm_botonePayco1" method="post" action="https://secure.payco.co/checkout.php"> 
+    <input name="p_cust_id_cliente" type="hidden" value="25223"/>
+    <input name="p_key" type="hidden" value="f36e9149f485659f0a1a9b14158d528175829a9e"/>
+    <input name="p_id_invoice" type="hidden" value=""/>
+    <input name="p_description" type="hidden" value="APOYO MUSICAL PROFESIONAL INDEPENDIENTE"/>
+    <input name="p_currency_code" type="hidden" value="USD"/>
+    <input name="p_amount" id="p_amount" type="hidden" value="20.00"/>
+    <input name="p_tax" id="p_tax" type="hidden" value="0"/>
+    <input name="p_amount_base" id="p_amount_base" type="hidden" value="0"/>
+    <input name="p_test_request" type="hidden" value="FALSE"/>
+    <input name="p_url_response" type="hidden" value=""/> 
+    <input name="p_url_confirmation" type="hidden" value=""/> 
+    <input name="p_signature" type="hidden" id="signature"  value="72f429d9d3cb82f15b5d303342d7d92f" />
+    <input name="idboton" type="hidden" id="idboton"  value="7256" />  
+    <input width="200" height="200" type="image" id="imagen" src="" />
+</form>
+
+	<!--30 DOLARES-->
+	<form align="center" id="frm_botonePayco2" name="frm_botonePayco" method="post" action="https://secure.payco.co/checkout.php"> 
+    <input name="p_cust_id_cliente" type="hidden" value="25223"/>
+    <input name="p_key" type="hidden" value="f36e9149f485659f0a1a9b14158d528175829a9e"/>
+    <input name="p_id_invoice" type="hidden" value=""/>
+    <input name="p_description" type="hidden" value="APOYO MUSICAL PROFESIONAL INDEPENDIENTE"/>
+    <input name="p_currency_code" type="hidden" value="USD"/>
+    <input name="p_amount" id="p_amount" type="hidden" value="30.00"/>
+    <input name="p_tax" id="p_tax" type="hidden" value="0"/>
+    <input name="p_amount_base" id="p_amount_base" type="hidden" value="0"/>
+    <input name="p_test_request" type="hidden" value="FALSE"/>
+    <input name="p_url_response" type="hidden" value=""/> 
+    <input name="p_url_confirmation" type="hidden" value=""/> 
+    <input name="p_signature" type="hidden" id="signature"  value="3f1e8635c2f687cb10a025dd8367886f" />
+    <input name="idboton" type="hidden" id="idboton"  value="7257" />  
+    <input width="200" height="200" type="image" id="imagen" src="" />
+</form>
+	<!--40 DOLARES-->
+	
+	<form align="center" id="frm_botonePayco3" name="frm_botonePayco" method="post" action="https://secure.payco.co/checkout.php"> 
+    <input name="p_cust_id_cliente" type="hidden" value="25223"/>
+    <input name="p_key" type="hidden" value="f36e9149f485659f0a1a9b14158d528175829a9e"/>
+    <input name="p_id_invoice" type="hidden" value=""/>
+    <input name="p_description" type="hidden" value=" APOYO MUSICAL PROFESIONAL INDEPENDIENTE"/>
+    <input name="p_currency_code" type="hidden" value="USD"/>
+    <input name="p_amount" id="p_amount" type="hidden" value="40.00"/>
+    <input name="p_tax" id="p_tax" type="hidden" value="0"/>
+    <input name="p_amount_base" id="p_amount_base" type="hidden" value="0"/>
+    <input name="p_test_request" type="hidden" value="FALSE"/>
+    <input name="p_url_response" type="hidden" value=""/> 
+    <input name="p_url_confirmation" type="hidden" value=""/> 
+    <input name="p_signature" type="hidden" id="signature"  value="39afe38e2f263130981cd5bd2b7faced" />
+    <input name="idboton" type="hidden" id="idboton"  value="7258" />  
+    <input type="image" id="imagen"  width="200" height="200" src="" />
+</form>
+	<!--50 DOLARES-->
+	
+	<form align="center" id="frm_botonePayco4" name="frm_botonePayco" method="post" action="https://secure.payco.co/checkout.php"> 
+    <input name="p_cust_id_cliente" type="hidden" value="25223"/>
+    <input name="p_key" type="hidden" value="f36e9149f485659f0a1a9b14158d528175829a9e"/>
+    <input name="p_id_invoice" type="hidden" value=""/>
+    <input name="p_description" type="hidden" value="  APOYO MUSICAL PROFESIONAL INDEPENDIENTE"/>
+    <input name="p_currency_code" type="hidden" value="USD"/>
+    <input name="p_amount" id="p_amount" type="hidden" value="50.00"/>
+    <input name="p_tax" id="p_tax" type="hidden" value="0"/>
+    <input name="p_amount_base" id="p_amount_base" type="hidden" value="0"/>
+    <input name="p_test_request" type="hidden" value="FALSE"/>
+    <input name="p_url_response" type="hidden" value=""/> 
+    <input name="p_url_confirmation" type="hidden" value=""/> 
+    <input name="p_signature" type="hidden" id="signature"  value="cbb5f4cea14ad03ced947c931c401cc3" />
+    <input name="idboton" type="hidden" id="idboton"  value="7259" />  
+    <input width="200" height="200" type="image" id="imagen" src="" />
+</form>
+	
+	<!--100 DOLARES-->
+	
+	
+	<form align="center" id="frm_botonePayco5" name="frm_botonePayco" method="post" action="https://secure.payco.co/checkout.php"> 
+    <input name="p_cust_id_cliente" type="hidden" value="25223"/>
+    <input name="p_key" type="hidden" value="f36e9149f485659f0a1a9b14158d528175829a9e"/>
+    <input name="p_id_invoice" type="hidden" value=""/>
+    <input name="p_description" type="hidden" value=" PAGO POR APOYO MUSICAL INDEPENDIENTE"/>
+    <input name="p_currency_code" type="hidden" value="USD"/>
+    <input name="p_amount" id="p_amount" type="hidden" value="100.00"/>
+    <input name="p_tax" id="p_tax" type="hidden" value="0"/>
+    <input name="p_amount_base" id="p_amount_base" type="hidden" value="0"/>
+    <input name="p_test_request" type="hidden" value="FALSE"/>
+    <input name="p_url_response" type="hidden" value=""/> 
+    <input name="p_url_confirmation" type="hidden" value=""/> 
+    <input name="p_signature" type="hidden" id="signature"  value="dc985b543df5f5c0bf49603ec88fd5fa" />
+    <input name="idboton" type="hidden" id="idboton"  value="7262" />  
+    <input width="200" height="200" type="image" id="imagen" src="" />
+</form>
